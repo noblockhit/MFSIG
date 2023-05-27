5f71789c303b621aa69617a18549da981b863007
noblockhit <67117651+noblockhit@users.noreply.github.com>
2023-05-27 15:43:09 +0200

    added function to remaining buttons

--- libs/deps/flask/static/script.js
+++ libs/deps/flask/static/script.js
@@ -24,6 +24,50 @@ function check_and_adjust_flex_orientation() {
     }
 }
 
+
+
+function update_end_pos() {
+    $.ajax({
+        url: "/microscope/end",
+        type: "GET",
+        success: function (data) {
+            $("#ending").html(`Ending: ${data}`) 
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function update_start_pos() {
+    $.ajax({
+        url: "/microscope/start",
+        type: "GET",
+        success: function (data) {
+            $("#start").html(`Start: ${data}`) 
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function update_curr_pos() {
+    console.log("getting curr position")
+    $.ajax({
+        url: "/microscope/current",
+        type: "GET",
+        success: function (data) {
+            $("#current").html(`Current: ${data}`) 
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+
+
 html = $("html");
 screen_aspect_ratio = html.width() / html.height();
 
@@ -41,6 +85,9 @@ $(window).on("load", () => {
         if (!mvActiveUp) return;
         $.ajax({
             url: `/microscope/up`,
+            success: function(data) {
+                $("#current").html(`Current: ${data}`) 
+            },            
             error: function (data) {
                 console.log(data);
             },
@@ -52,6 +99,9 @@ $(window).on("load", () => {
         if (!mvActiveDown) return;
         $.ajax({
             url: `/microscope/down`,
+            success: function(data) {
+                $("#current").html(`Current: ${data}`) 
+            },            
             error: function (data) {
                 console.log(data);
             },
@@ -61,12 +111,15 @@ $(window).on("load", () => {
     // up button
     mdUpEvt = false;
     mvUpInterval = null;
-    $("#move-up").on("mousedown pointerdown", function () {
+    $("#move-up").on("pointerdown", function () {
         if (mdUpEvt) return;
         mdUpEvt = true;
         mvActiveUp = true;
         $.ajax({
             url: "/microscope/up",
+            success: function(data) {
+                $("#current").html(`Current: ${data}`) 
+            },            
             error: function (data) {
                 console.log(data);
             },
@@ -87,12 +140,15 @@ $(window).on("load", () => {
 
     mdDownEvt = false;
     mvDownInterval = null;
-    $("#move-down").on("mousedown pointerdown", function () {
+    $("#move-down").on("pointerdown", function () {
         if (mdDownEvt) return;
         mdDownEvt = true;
         mvActiveDown = true;
         $.ajax({
             url: "/microscope/down",
+            success: function(data) {
+                $("#current").html(`Current: ${data}`) 
+            },            
             error: function (data) {
                 console.log(data);
             },
@@ -108,6 +164,31 @@ $(window).on("load", () => {
         mvActiveDown = false;
         clearInterval(mvDownInterval);
     });
+
+    // manual button updates
+    $("#current").on("pointerdown", update_curr_pos);
+    $("#start").on("pointerdown", update_start_pos);
+    $("#ending").on("pointerdown", update_end_pos);
+    
+    
+    $("#set-start").on("pointerdown", () => {
+        $.post("/microscope/start", async=false);
+        update_start_pos()
+    });
+    $("#set-ending").on("pointerdown", () => {
+        $.post("/microscope/end", async=false);
+        update_end_pos()
+    });
+
+    $("#move-start").on("pointerdown", () => {
+        $.get("/microscope/move/start", async=false);
+        update_curr_pos()
+    });
+    $("#move-ending").on("pointerdown", () => {
+        $.get("/microscope/move/end", async=false);
+        update_curr_pos()
+    });
 });
+
 
 addEventListener("resize", check_and_adjust_flex_orientation);
