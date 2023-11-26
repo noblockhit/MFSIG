2b0a4891aa738f58481199af39b92ed228801f15
noblockhit <67117651+noblockhit@users.noreply.github.com>
2023-11-26 18:11:42 +0100

    added safety for missing whatsapp info

--- libs/notifier.py
+++ libs/notifier.py
@@ -2,7 +2,7 @@ import requests
 from .state import State
 
 def send_text_to_whatsapp(message):
-    if State.whatsapp_number == "" or State.whatsapp_api_key == -1:
+    if State.whatsapp_number == "" or State.whatsapp_api_key == "":
         return
     
     message = message.replace(" ", "+")
